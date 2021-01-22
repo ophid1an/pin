3bec010c8af163a997a67584d9836bab358b2638
ophid1an <ophid1an@users.noreply.github.com>
2021-01-22 12:20:39 +0200

    changes

---
+++ theone.php
@@ -0,0 +1,11 @@
+<?php
+
+$persons = include __DIR__.'/persons.php';
+
+// There are persons, with names and years of birth
+var_dump($persons);
+
+// From these persons will are looking for the one
+// with 4 letters in their name and with age closest to 22
+$one = '???';
+var_dump($one);
