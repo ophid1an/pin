c41b82d846593f57ad57a3aa63d5e84befc32e3b
ophid1an <ophid1an@users.noreply.github.com>
2021-01-22 10:46:17 +0200

    first commit

---
+++ manip.php
@@ -0,0 +1,11 @@
+<?php
+
+$people = include __DIR__.'/persons.php';
+
+var_dump($people);
+
+// Filter persons that have only 4 letters in their name
+$filteredPeople = ???
+
+// From the filtered persons, output the name of the person that has age closest to 22
+$one = ???
---
+++ persons.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    [
+        'id' => 3,
+        'name' => 'John',
+        'yearOfBirth' => 1980,
+    ],
+    [
+        'id' => 5,
+        'name' => 'Helen',
+        'yearOfBirth' => 1998,
+    ],
+    [
+        'id' => 6,
+        'name' => 'Mary',
+        'yearOfBirth' => 2000,
+    ],
+    [
+        'id' => 8,
+        'name' => 'Alex',
+        'yearOfBirth' => 1997,
+    ],
+    [
+        'id' => 12,
+        'name' => 'Nick',
+        'yearOfBirth' => 1985,
+    ],
+];
